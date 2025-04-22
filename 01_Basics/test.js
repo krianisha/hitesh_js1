77c7a9f433f935143cb6fc9dce088011b3865383
krianisha <126376286+krianisha@users.noreply.github.com>
2025-04-22 11:21:19 +0000

    first commit

---
+++ 01_Basics/test.js
@@ -0,0 +1,11 @@
+const eid=123
+let ename="Anisha"
+var eaccount="xxx34xxx56"
+esalary="200000"  //create implicitly global variable
+
+//eid=12;
+
+
+//we donot use var because it donot give block scope and functional scope
+console.log(eid);
+console.table([eid,ename,eaccount,esalary])
