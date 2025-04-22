cc3f739ea428c546de54ed17c5275a9830e3813b
krianisha <126376286+krianisha@users.noreply.github.com>
2025-04-22 11:57:53 +0000

    conversion

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,5 @@
+let a=33;
+a=String(a);
+a=Boolean(a);
+console.log(a);
+console.log(typeof(a));
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -2,6 +2,12 @@ const eid=123
 let ename="Anisha"
 var eaccount="xxx34xxx56"
 esalary="200000"  //create implicitly global variable
+let age; //undefined;
+/*
+NULL is a object;
+undefined is a data type
+*/
+//"use strict"  read code as new js syntax or standarad
 
 //eid=12;
 
