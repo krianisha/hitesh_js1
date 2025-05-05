c91ac05296b77ae8bfcba9ff74ceb402ef43d9b0
krianisha <126376286+krianisha@users.noreply.github.com>
2025-05-05 11:44:40 +0000

    call

--- class_oops/call.js
+++ class_oops/call.js
@@ -5,7 +5,7 @@ function SetUsername(username){
 }
 
 function createUser(username, email, password){
-    SetUsername.call(this, username)
+    SetUsername.call(this, username)    //apna this diya takki usme set ho
    
     this.email = email
     this.password = password
