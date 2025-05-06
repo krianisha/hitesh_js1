5e423d582ae1fafe1a3b1149337b65d607a55b2b
krianisha <126376286+krianisha@users.noreply.github.com>
2025-05-06 04:58:21 +0000

    d

--- class_oops/myclasses.js
+++ class_oops/myclasses.js
@@ -1,21 +1,21 @@
-//ES6
+// ES6
 
-class User{
-    constructor(username,email,password){
-        this.username=username
-        this.email=email
-        this.password=password
-    }
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-    changeusername(){
-        return `${this.username.toUpperCase()}`
-    }
-}
-const ani=new User('Anisha','anisha123@gmail.com','123')
-console.log(ani.encryptPassword());
-console.log(ani.changeusername());
+// class User{
+//     constructor(username,email,password){
+//         this.username=username
+//         this.email=email
+//         this.password=password
+//     }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeusername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+// const ani=new User('Anisha','anisha123@gmail.com','123')
+// console.log(ani.encryptPassword());
+// console.log(ani.changeusername());
 
 
 // behind the scene
@@ -26,10 +26,10 @@ function User(username, email, password){
     this.password = password
 }
 
-User.prototype.encryptPassword(){
+User.prototype.encryptPassword=function(){
     return `${this.password}abc`
 }
-User.prototype.changeUsername(){
+User.prototype.changeUsername=function(){
     return `${this.username.toUpperCase()}`
 }
 
