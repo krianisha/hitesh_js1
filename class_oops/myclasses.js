d7d21a88e05343f5dada3743abc178a883d156d1
krianisha <126376286+krianisha@users.noreply.github.com>
2025-05-06 04:56:58 +0000

    constructor

---
+++ class_oops/myclasses.js
@@ -0,0 +1,40 @@
+//ES6
+
+class User{
+    constructor(username,email,password){
+        this.username=username
+        this.email=email
+        this.password=password
+    }
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeusername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+const ani=new User('Anisha','anisha123@gmail.com','123')
+console.log(ani.encryptPassword());
+console.log(ani.changeusername());
+
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
